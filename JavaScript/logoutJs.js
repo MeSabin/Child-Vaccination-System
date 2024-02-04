d3dd6517724fc9b7cc2659c84c9c5ff34c8b84ca
Sabin Kaphle <kaphlesabin789@gmail.com>
2024-02-04 12:05:49 +0545

    js file was added for logout button trigger

---
+++ JavaScript/logoutJs.js
@@ -0,0 +1,19 @@
+
+          function showModal(){
+            document.querySelector('.overlay').classList.add('showOverlay');
+            document.querySelector('.confirmLogout').classList.add('showLogout');
+          }
+          function closeModal(){
+            document.querySelector('.overlay').classList.remove('showOverlay');
+            document.querySelector('.confirmLogout').classList.remove('showLogout');
+          }
+
+          const btnLogin= document.querySelector('.profile');
+          btnLogin.addEventListener("click",showModal);
+
+          const cancelLogout= document.querySelector('.cancelbutton');
+          cancelLogout.addEventListener("click",closeModal);
+          
+          const clickToCancel= document.querySelector('.overlay');
+          clickToCancel.addEventListener("click",closeModal);
+         
