5215d78d92082d56a3420571fff789f72b61b3c3
Sabin Kaphle <kaphlesabin789@gmail.com>
2024-03-13 08:56:59 +0545

    message ui changed

--- HtmlFiles/404NotFound.php
+++ HtmlFiles/404NotFound.php
@@ -14,14 +14,23 @@
         .notFoundMsg {
             border-radius: 5px;
             padding: 20px;
-            margin-top: 250px;
+            margin-top: 220px;
             text-align:center;
+        }
+        h1{
+            font-size:5rem;
+            color: rgb(255, 98, 98);
+            margin:-20px;
+        }
+        h3,p{
+            color: rgb(88, 88, 88);
         }
     </style>
 </head>
 <body>
     <div class="notFoundMsg">
-        <h1>404 Not Found</h1>
+        <h1>404</h1>
+        <h3>Not Found</h3>
         <p>The page you requested could not be found.</p>
     </div>
 </body>
